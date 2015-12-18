357216a4574d9b9323b9c90007c0696ec80de111
chenbjin <chenbjin@gmail.com>
2015-12-18 17:04:10 +0800

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// main.cpp
+//
+// Created by Chenbjin on 2015/12/18.
+// Copyright(C) 2015, Chenbjin. All rights reserved.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
