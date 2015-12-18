bc7a51e0d89e1efc2d447928b81e49a5011ddede
chenbjin <chenbjin@gmail.com>
2015-12-18 17:46:39 +0800

    main.cpp

---
+++ BeetleDB/main.cpp
@@ -0,0 +1,14 @@
+//
+// main.cpp
+//
+// Created by Chenbjin on 2015/12/18.
+// Copyright(C) 2015, Chenbjin. All rights reserved.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
