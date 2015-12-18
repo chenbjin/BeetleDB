40282ebcb1c51ef323e2dbb952ff5dbd72c78033
chenbjin <chenbjin@gmail.com>
2015-12-18 17:56:12 +0800

    add Interpreter

---
+++ BeetleDB/Interpreter.cpp
@@ -0,0 +1,17 @@
+//
+// Interpreter.cpp
+//
+// Created by Chenbjin on 2015/12/18.
+// Copyright(C) 2015, Chenbjin. All rights reserved.
+//
+#include "Interpreter.h"
+
+
+Interpreter::Interpreter(void)
+{
+}
+
+
+Interpreter::~Interpreter(void)
+{
+}
---
+++ BeetleDB/Interpreter.h
@@ -0,0 +1,14 @@
+//
+// Interpreter.h
+//
+// Created by Chenbjin on 2015/12/18.
+// Copyright(C) 2015, Chenbjin. All rights reserved.
+//
+#pragma once
+class Interpreter
+{
+public:
+	Interpreter(void);
+	~Interpreter(void);
+};
+
